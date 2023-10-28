0f0a78b159bfd4885bf9e80628d2fef516718660
¨juan <jecb1913@gmail.com>
2023-10-28 11:57:21 -0500

    Inicio del proyecto, encuestas.

---
+++ src/test/java/com/co/Apirestencuestas/ApiRestEncuestasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.co.Apirestencuestas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestEncuestasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
